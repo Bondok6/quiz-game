482edfe04a9b8240c995c894632c90f492e622e2
Bondok6 <kirolousbondok6@gmail.com>
2022-10-16 20:14:57 +0200

    Finish Quiz Game

---
+++ src/components/GameStateFinish.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="modal" style="text-align: center">
+    <h2>{{ uiState === 'won' ? 'You Won!' : 'You Lost!' }}</h2>
+    <h3>{{ uiState === 'won' ? '🏆' : '😞' }}</h3>
+    <button @click="reset()">Let's play again!</button>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  methods: {
+    reset() {
+      this.$store.commit('reset');
+    },
+  },
+  computed: {
+    ...mapState(['uiState']),
+  },
+};
+</script>
+
+<style></style>
